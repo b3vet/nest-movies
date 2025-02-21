28dd003df4837631ca4f8e9b05a04f44c9db470d
Berke Ucvet <berke.ucvet@trendyol.com>
2025-02-21 03:20:48 +0300

    feat: Make swagger public

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,16 @@
 import { Controller, Get, Injectable, Redirect } from "@nestjs/common";
+import { Public } from "./auth/public.decorator";
 
 @Injectable()
 @Controller()
 export class AppController {
 	@Get("/")
 	@Redirect("/docs")
+	@Public()
 	async redirectToDocs() {}
 
 	@Get("/health")
+	@Public()
 	async healthCheck() {
 		return {
 			status: "ok",
